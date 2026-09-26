2e4e17a5c8870a045d952e598480c85ce38ca6fa
Juliano Felisberto <julianoeloi1@gmail.com>
2026-09-26 13:43:24 -0300

    20260926134311_criando_componente_logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
       <div className='container-fluid'>
         <Container>
-          <Heading>LOGO</Heading>
+          <Logo />
         </Container>
 
         <Container>
