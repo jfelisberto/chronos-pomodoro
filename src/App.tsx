adf49cc7d83725350a7b5f306d6a0706ac31bae2
Juliano Felisberto <julianoeloi1@gmail.com>
2026-09-26 10:28:28 -0300

    20260926102751_css_reset_vars_theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { TimerIcon } from 'lucide-react';
 import { Heading } from './components/Heading';
 
 export function App() {
-  console.log('oi');
-
   return (
     <>
-      <Heading />
+      <Heading>
+        Olá mundo, React
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+      <Heading>Mussum impsum</Heading>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero non eligendi ratione voluptas at aspernatur
         porro! Quisquam, debitis. Aut voluptate necessitatibus animi aperiam eius quis rem quas inventore totam
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,11 @@
+import type React from 'react';
 import styles from './Heading.module.css';
 
-export function Heading() {
-  console.log(styles);
-  return <h1 className={styles.heading}>Ola mundo!</h1>;
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading(props: HeadingProps) {
+  console.log(styles, props);
+  return <h1 className={styles.heading}>{props.children}</h1>;
 }
